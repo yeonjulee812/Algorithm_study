a988386f574605107e9b3a5bd6caca05897b6df9
이연주 <116277985+yeonjulee812@users.noreply.github.com>
2022-12-19 14:40:33 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -0,0 +1,10 @@
+a = int(input())
+for i in range(a):
+    scores = list(map(int, input().split()))
+    avg = sum(scores[1:])/scores[0]
+    cnt = 0
+    for i in scores[1:]:
+        if i > avg:
+            cnt += 1
+    per = (cnt/scores[0])*100
+    print('%.3f' %per + '%')
