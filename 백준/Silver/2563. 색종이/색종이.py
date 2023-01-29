72ef0ce409359288d006ddb677d252872bd3f5cc
이연주 <116277985+yeonjulee812@users.noreply.github.com>
2023-01-29 23:49:46 +0900

    [Silver V] Title: 색종이, Time: 80 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,17 @@
+T = int(input())
+white_board = [[0]*100 for _ in range(100)]
+
+for _ in range(T):
+    a, b = map(int, input().split())
+    
+    for i in range(b, b+10):
+        for j in range(a, a+10):
+            white_board[i][j] = 1
+
+cnt = 0
+for i in range(100):
+    for j in range(100):
+        if white_board[i][j]:
+            cnt += 1
+
+print(cnt)
