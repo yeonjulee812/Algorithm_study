cb0caa1fa4fdb18e5e506081cd2c0554774e015a
이연주 <116277985+yeonjulee812@users.noreply.github.com>
2023-02-13 00:26:46 +0900

    [Silver III] Title: 문자열 집합, Time: 872 ms, Memory: 42384 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,12 +1,7 @@
 N, M = map(int, input().split())
 
-s = set()
-for _ in range(N):
-    s.add(input())
-
-test = []
-for _ in range(M):
-    test.append(input())
+s = set(input() for _ in range(N)) # 집합 S에 같은 문자열이 여러 번 주어지는 경우는 없음
+test = [input() for _ in range(M)]
 
 cnt = 0
 for i in test:
