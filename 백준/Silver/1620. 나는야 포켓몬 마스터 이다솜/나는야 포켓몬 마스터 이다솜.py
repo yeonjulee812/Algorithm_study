1344edafe4c84643b7ea2dffaa83dc680f615fc3
이연주 <116277985+yeonjulee812@users.noreply.github.com>
2023-02-12 18:47:33 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 268 ms, Memory: 51396 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -0,0 +1,20 @@
+from sys import stdin
+def input():
+    return stdin.readline().rstrip()
+
+N, M = map(int, input().split())
+
+by_id = {}
+by_name = {}
+
+for i in range(1, N+1):
+    pokemon = input()
+    by_id[i] = pokemon
+    by_name[pokemon] = i
+ 
+for _ in range(M):
+    x = input()
+    if x.isdigit():
+        print(by_id[int(x)])
+    else:
+        print(by_name[x])
