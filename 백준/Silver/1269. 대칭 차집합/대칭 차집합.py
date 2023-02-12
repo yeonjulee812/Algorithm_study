b95de995db0c25547d112d403ed2034d493ccf2e
이연주 <116277985+yeonjulee812@users.noreply.github.com>
2023-02-13 01:30:58 +0900

    [Silver IV] Title: 대칭 차집합, Time: 244 ms, Memory: 88612 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -2,4 +2,4 @@ A, B = map(int, input().split())
 A_set = set(map(int, input().split()))
 B_set = set(map(int, input().split()))
 
-print(len(A_set.difference(B_set)) + len(B_set.difference(A_set)))
+print(len(A_set^B_set))
