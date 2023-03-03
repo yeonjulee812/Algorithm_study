7d84eb06e4856ced0ac7238b48bf87673fa32045
이연주 <116277985+yeonjulee812@users.noreply.github.com>
2023-03-03 10:27:01 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 576 ms, Memory: 60932 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+
+N, M, R = map(int, sys.stdin.readline().strip().split())
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    spot, node = map(int, sys.stdin.readline().strip().split())
+    graph[spot].append(node)
+    graph[node].append(spot)
+
+for i in range(N+1):
+    graph[i].sort(reverse=1)
+
+visited = [0] * (N+1)
+queue = deque([R])
+cnt = 1
+visited[R] = cnt
+while queue:
+    i = queue.popleft()
+    for j in graph[i]:
+        if not visited[j]:
+            cnt += 1
+            visited[j] = cnt
+            queue.append(j)
+print(*visited[1:], sep='\n')
+
