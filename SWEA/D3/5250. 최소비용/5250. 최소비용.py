78737e94afea5dae27888147989faafb9ee04ec0
이연주 <116277985+yeonjulee812@users.noreply.github.com>
2023-04-04 14:53:16 +0900

    Update 5250. 최소비용.py

--- SWEA/D3/5250. 최소비용/5250. 최소비용.py
+++ SWEA/D3/5250. 최소비용/5250. 최소비용.py
@@ -1,3 +1,4 @@
+# 방법 1 - 3,721 ms
 def dij(N):
     INF = 100000
     D = [[INF]*N for _ in range(N)]
@@ -26,5 +27,26 @@ T = int(input())
 for tc in range(1,T+1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
-    dij(N)
     print(f'#{tc} {dij(N)}')
+
+# 방법 2 - 263 ms
+def f(N):
+    INF = 1000000
+    Q = []
+    D = [[INF]*N for _ in range(N)]
+    Q.append((0,0))
+    D[0][0] = 0
+    while Q:
+        i, j = Q.pop(0)
+        for di, dj in [[0,1],[1,0],[0,-1],[-1,0]]:
+            ni, nj = i+di, j+dj
+            if 0<=ni<N and 0<=nj<N and D[ni][nj]>(D[i][j]+1+max(0,arr[ni][nj]-arr[i][j])):
+                D[ni][nj] = D[i][j]+1+max(0,arr[ni][nj]-arr[i][j])
+                Q.append((ni,nj))
+    return D[N-1][N-1]
+
+T = int(input())
+for tc in range(1,T+1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    print(f'#{tc} {f(N)}')
