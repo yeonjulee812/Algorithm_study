616dda584836e9d490ec13cddb33363f4cfaf34d
이연주 <116277985+yeonjulee812@users.noreply.github.com>
2023-04-25 20:53:59 +0900

    Update 좋다.py

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -1,20 +1,19 @@
-import sys
-input = sys.stdin.readline
-
 N = int(input())
+li = list(map(int, input().split()))
+li.sort()
+cnt = 0
 
-arr = list(map(int, input().split()))
-if sum(arr):        # 모두 0 이면 답은 N
-    hash = {}
-    for i in range(N):      # 인덱스 저장
-      hash[arr[i]] = i
-    cnt = 0
-    for i in range(N):
-        for j in range(N):
-          add = arr[i] - arr[j] 
-          if i!= j and add in hash and hash[add] != i and hash[add] != j:       # 자신이 아닌 숫자와 뺐을 때, 값이 뺀값과 자신이 아니면서 hash내부에 있으면 ++
+for i in range(N):
+    new_li = li[:i] + li[i+1:]
+    l, r = 0, N-2
+    while l < r:
+        s = new_li[l] + new_li[r]
+        if li[i] == s:
             cnt += 1
-            break       # 하나만 있어도 좋은 수
-    print(cnt)
-else:
-    print(N)
+            break
+        if li[i] < s:
+            r -= 1
+        elif li[i] > s:
+            l += 1
+
+print(cnt)
